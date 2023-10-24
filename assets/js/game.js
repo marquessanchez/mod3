b53984e60aa11fc74cc47c22aa011fdf23e92234
mrican217@gmail.com <mrican217@gmail.com>
2023-10-24 14:35:40 -0400

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,33 @@
+  var playerName = window.prompt("What is your robot's name?");
+  var playerHealth = 100;
+  var playerAttack = 10;
+
+  console.log(playerName, playerHealth, playerAttack);
+
+  var enemyName = "Roberto";
+  var enemyHealth = 50;
+  var enemyAttack = 12;
+
+  var fight = function() {
+    // window.alert("Welcome to Robot Gladiators!");
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+    }
+    else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    if (playerHealth <= 0) {
+      window.alert(playerHealth + " has died!");
+    }
+    else {
+      window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+    
+  }
+  fight();
